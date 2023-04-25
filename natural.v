136c6165d4c9064d07e09732286b7bd705d1eb93
Hidden-me <zrway43@qq.com>
2023-04-25 23:43:16 +0800

    + definition of natural numbers + plus and commutativity proof

---
+++ natural.v
@@ -0,0 +1,58 @@
+Module Natural.
+
+Inductive num : Type :=
+| z
+| s (n : num).
+
+Fixpoint plus (n m : num) : num :=
+    match m with
+    | z => n
+    | s m' => s (plus n m')
+    end.
+
+Lemma plus_succ_comm :
+    forall n m : num, plus (s n) m = s (plus n m).
+Proof.
+    induction m as [| m' IH].
+    {
+        reflexivity.
+    }
+    {
+        simpl.
+        rewrite IH.
+        reflexivity.
+    }
+Qed.
+
+Lemma plus_zero :
+    forall n : num, plus z n = n.
+Proof.
+    induction n as [| n' IH].
+    {
+        reflexivity.
+    }
+    {
+        simpl.
+        rewrite IH.
+        reflexivity.
+    }
+Qed.
+
+Theorem plus_comm :
+    forall n m : num, plus n m = plus m n.
+Proof.
+    induction m as [| m' IH].
+    {
+        simpl.
+        symmetry.
+        apply plus_zero.
+    }
+    {
+        simpl.
+        rewrite plus_succ_comm.
+        rewrite IH.
+        reflexivity.
+    }
+Qed.
+
+End Natural.
